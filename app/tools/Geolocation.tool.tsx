512523ea14aed1d866236869c04d5da40c6b113c
TheAlan404 <gokdenizbektas@gmail.com>
2025-09-28 07:37:27 +0000

    geolocation: bugfix & gmaps embed

--- app/tools/Geolocation.tool.tsx
+++ app/tools/Geolocation.tool.tsx
@@ -1,11 +1,14 @@
 import type { ToolInfo } from "../tool";
-import { Box, Button, Divider, Group, Loader, Stack, Text, Tooltip } from "@mantine/core";
+import { Anchor, Box, Button, Divider, Group, Loader, Stack, Text, Tooltip } from "@mantine/core";
 import { useWebPermission } from "../hooks/web/useWebPermission";
 import { useState } from "react";
 import { useAsyncFn } from "../hooks/utils/useAsyncFn";
 import { IconMapPin, IconWorldPin } from "@tabler/icons-react";
 import { PermissionNugget } from "../components/ui/PermissionNugget";
 import { ResultsTable } from "../components/output/ResultsTable";
+
+// pls no steal ty
+export const GMAPS_API_KEY = "AIzaSyAb9PxKDrSJWR1YV1se28GeYzLpnNc7hZ4";
 
 export const info: ToolInfo = {
     id: "geolocation",
@@ -28,9 +31,7 @@ export default function GeolocationTool() {
         return pos;
     });
 
-    const [watching, setWatching] = useState(false);
-
-
+    const [updateInterval, setUpdateInterval] = useState<number>(0);
 
     return (
         <Stack>
@@ -69,14 +70,45 @@ export default function GeolocationTool() {
                             ["timestamp", position.timestamp.toString()],
                             ["latitude", position.coords.latitude.toString()],
                             ["longitude", position.coords.longitude.toString()],
-                            position.coords.speed !== null ? ["speed", position.coords.speed.toString()] : null,
+                            position.coords.speed != null ? ["speed", position.coords.speed.toString()] : null,
                             ["accuracy", position.coords.accuracy.toString()],
-                            position.coords.altitude !== null ? ["altitude", position.coords.altitude.toString()] : null,
+                            position.coords.altitude != null ? ["altitude", position.coords.altitude.toString()] : null,
                             position.coords.altitudeAccuracy !== null ? ["altitude", position.coords.altitudeAccuracy.toString()] : null,
-                            position.coords.heading !== null ? ["altitude", position.coords.heading.toString()] : null,
+                            position.coords.heading != null ? ["altitude", position.coords.heading.toString()] : null,
                         ]}
                     />
                 )}
+            </Stack>
+
+            {position?.coords && (
+                <Stack>
+                    <iframe
+                        src={`https://www.google.com/maps/embed/v1/place?${new URLSearchParams([
+                            ["key", GMAPS_API_KEY],
+                            ["q", [position.coords.latitude, position.coords.longitude].join(",")],
+                        ])}`}
+                        style={{
+                            border: 0,
+                            width: "100%",
+                            height: "100%",
+                            borderRadius: "var(--mantine-radius-md)",
+                            backgroundColor: "white",
+                        }}
+                        loading="lazy"
+                    />
+                </Stack>
+            )}
+
+            <Stack ta="center">
+                <Text inherit fz="sm" c="dimmed">
+                    This tool uses the <Anchor
+                        inline
+                        inherit
+                        href="https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API"
+                    >
+                        Geolocation API
+                    </Anchor>
+                </Text>
             </Stack>
         </Stack>
     )
